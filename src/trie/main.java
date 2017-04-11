90806ab37c250c6ff093a640b9f392b57adeec13
Olzhas Kaiyrakhmet <olzhabay.i@gmail.com>
2017-04-11 17:24:04 +0900

    fixed testing

--- src/trie/main.java
+++ src/trie/main.java
@@ -7,12 +7,21 @@ public class main {
     public static void main(String[] args) {
         /** OPTIONAL **/
         // Test freely whether your implementation is correct.
-        Trie trie = new Trie(2);
-        trie.add((ItemSet) new ArrayList<>(Arrays.asList(1, 2)));
-        trie.add((ItemSet) new ArrayList<>(Arrays.asList(1, 3)));
-        trie.add((ItemSet) new ArrayList<>(Arrays.asList(2, 3)));
+        ItemSet itemset1 = new ItemSet(2);
+        itemset1.addAll(Arrays.asList(1, 2));
 
-        ArrayList<Integer> transaction = new ArrayList<>(Arrays.asList(1, 2, 3, 4 ,5));
+        ItemSet itemset2 = new ItemSet(2);
+        itemset2.addAll(Arrays.asList(1, 3));
+
+        ItemSet itemset3 = new ItemSet(2);
+        itemset3.addAll(Arrays.asList(2, 3));
+
+        Trie trie = new Trie(2);
+        trie.add(itemset1);
+        trie.add(itemset2);
+        trie.add(itemset3);
+
+        ArrayList<Integer> transaction = new ArrayList<>(Arrays.asList(2, 3, 4 ,5));
         ArrayList<ItemSet> matchedItemSet = new ArrayList<>();
         trie.findItemSets(matchedItemSet, trie.getRootNode(), transaction, 0);
 
